b3fc6039cab9f4fbee7714e442c753f69c968681
m1st0rm <mishaboy333@gmail.com>
2024-09-12 13:02:24 +0300

    Added evil_arthas_fortune_wheel.py

---
+++ Lab_1/evil_arthas_fortune_wheel.py
@@ -0,0 +1,26 @@
+from typing import Tuple
+
+
+class FortuneWheel:
+    def __init__(self):
+        self.games = {}
+
+    def donate(self, donation: Tuple[str, float]):
+        game_name, game_score = donation
+
+        if game_name in self.games:
+            self.games[game_name] += game_score
+        else:
+            self.games[game_name] = game_score
+
+    def get_games_with_probabilities(self):
+        if len(self.games) == 0:
+            raise ConnectionError("Games list is empty")
+
+        games_sum = sum(self.games.values())
+
+        games_with_probabilities = {game: score / games_sum for game, score in self.games.items()}
+
+        return games_with_probabilities
+
+
